822e6f342319adaab22b5f60c2c7bbcb7ee245a9
mahmoodhamdi <hmdy7486@gmail.com>
2024-07-20 09:46:39 +0300

    Add utility functions and themes to the utils folder
    - Implemented HelperFunctions with methods for date formatting, screen size, dark mode detection, and navigation.
    - Added AppTheme with light and dark themes, and various UI element themes.
    - Created Validator and Formatter classes for input validation and formatting.
    - Added constants, enums, Sizes, AppColors, and placeholders for API constants and image strings.
    - Implemented LoggerHelper and DeviceUtils for device-specific utilities and logging.
    - Updated main.dart to use the newly added themes and system theme mode.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,6 +5,10 @@
 import FlutterMacOS
 import Foundation
 
+import connectivity_plus
+import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
